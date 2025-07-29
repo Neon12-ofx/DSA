8a4d350f56affe354f05f65503aac3d1a9c87a12
203Subhra <subhrakantidas523@gmail.com>
2025-07-29 18:20:35 +0530

    adding new files to List

---
+++ List(Array)/Maximum_subarray_sum.py
@@ -0,0 +1,12 @@
+#Brute for tc-->O(N^2)
+def maxi_sub_sum(arr):
+    maxi=float('-inf')
+    for i in range(0,len(arr)):
+        total = 0   #Each time you start a new subarray from index i, you need a fresh total.
+                    # If you don’t reset total, then it will keep accumulating values from previous subarrays, which gives incorrect results.
+        for j in range(i,len(arr)):
+            total=total+arr[j]
+            maxi=max(maxi,total)
+    return maxi
+
+print(maxi_sub_sum([1,2,3,4,5]))
