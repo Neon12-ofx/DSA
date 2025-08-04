4c60fea29b8e7474575cd5f147eb29dc07ba920b
203Subhra <subhrakantidas523@gmail.com>
2025-08-05 00:36:43 +0530

    adding new folder for binary search and adding new file

---
+++ Binary Search/lower_bound.py
@@ -0,0 +1,19 @@
+
+def lower_b(arr,target):
+    #[1 1 1 2 3 3 3 5 6 7 7 7 9 12 12 13]
+    lb=0
+    n=len(arr)
+    l=0
+    h=n-1
+    while l<h:
+        m=(l+h)//2
+        if arr[m]<=target:   #codn for arr[m]>=target
+            lb=m  #store the lowest index
+            h=m-1
+        else:
+            l=m+1
+    return lb
+
+n=list(map(int,input().split()))
+x=int(input())
+print(lower_b(n,x))
